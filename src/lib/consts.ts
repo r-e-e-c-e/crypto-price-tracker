382574065b1d7fb6daf9b394cfcbc96df81eabcc
Reece Goddard <reecegoddard@me.com>
2024-12-20 20:13:05 +0000

    Add consts file.

---
+++ src/lib/consts.ts
@@ -0,0 +1,5 @@
+export const COINBASE_PRODUCTS_URL = 'https://api.exchange.coinbase.com/products';
+export const LIST_DATA_KEY = 'stored-list-data';
+export const LIST_LOCAL_STORAGE_KEY = 'list-items';
+export const LOCAL_STORAGE_EXPIRY = 1000 * 60 * 5; // 5m
+export const LOCAL_STORAGE_THROTTLE = 1000 * 30; // 30s
