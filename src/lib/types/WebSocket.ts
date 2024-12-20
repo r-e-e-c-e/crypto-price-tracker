1a43f3022a14f90bd86e948a2f5a6a19aded9388
Reece Goddard <reecegoddard@me.com>
2024-12-20 20:13:29 +0000

    Add types for Coinbase WebSocket.

---
+++ src/lib/types/WebSocket.ts
@@ -0,0 +1,35 @@
+export type WebSocketState = {
+	connected: boolean;
+	messages: CoinbaseMessage[];
+	error: string | null;
+};
+
+export type TickerMessage = {
+	type: 'ticker';
+	sequence: number;
+	product_id: string;
+	price: string;
+	open_24h: string;
+	volume_24h: string;
+	low_24h: string;
+	high_24h: string;
+	volume_30d: string;
+	best_bid: string;
+	best_ask: string;
+	side: 'buy' | 'sell';
+	time: string; // ISO 8601 timestamp
+	trade_id: number;
+	last_size: string;
+};
+
+export type SubscriptionConfirmation = {
+	type: 'subscriptions';
+	channels: Channel[];
+};
+
+export type Channel = {
+	name: string;
+	product_ids: string[];
+};
+
+export type CoinbaseMessage = TickerMessage | SubscriptionConfirmation;
