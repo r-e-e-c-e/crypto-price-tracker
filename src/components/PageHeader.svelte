3700e26bf37a8bce904bee4f45f5e901f0162484
Reece Goddard <reecegoddard@me.com>
2024-12-21 01:42:44 +0000

    Move page header into own component.

---
+++ src/components/PageHeader.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { MouseEventHandler } from 'svelte/elements';
+
+	export let onInvestClick: MouseEventHandler<HTMLButtonElement>;
+</script>
+
+<header class="page-header">
+	<h1 class="page-title">Crypto Price Tracker</h1>
+	<button onclick={onInvestClick}>Invest</button>
+</header>
+
+<style lang="scss">
+	.page-header {
+		display: flex;
+		justify-content: space-between;
+
+		.page-title {
+			font-family: 'Castoro', Georgia, 'Times New Roman', Times, serif;
+			margin: 0;
+		}
+	}
+</style>
